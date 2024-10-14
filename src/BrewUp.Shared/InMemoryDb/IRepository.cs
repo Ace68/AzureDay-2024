13bf8f2288786d8a4e3a61d69abf6b178a5de8b9
Alberto Acerbis <alberto.acerbis@gmail.com>
2024-10-14 18:22:36 +0200

    Use .NET Aspire and Aspire.AzureServiceBus

---
+++ src/BrewUp.Shared/InMemoryDb/IRepository.cs
@@ -0,0 +1,9 @@
+﻿using BrewUp.Shared.Models;
+
+namespace BrewUp.Shared.InMemoryDb;
+
+public interface IRepository
+{
+    void AddSalesOrder(SalesOrder salesOrder);
+    IEnumerable<SalesOrder> GetSalesOrders();
+}
